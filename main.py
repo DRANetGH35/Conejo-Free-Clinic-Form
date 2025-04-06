de3953fc64ae62f589da7f8bd21031df2693ba61
DRANetGH35 <devanaptaker@gmail.com>
2025-04-05 17:44:53 -0700

    login page ready

--- main.py
+++ main.py
@@ -4,20 +4,26 @@ from flask_bootstrap import Bootstrap
 from flask_ckeditor import CKEditor
 from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
 from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
 from sqlalchemy import Integer, String, Text
-from functools import wraps
 from werkzeug.security import generate_password_hash, check_password_hash
-
+from wtforms.fields.simple import PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
-#app.config['SECRET_KEY'] = ''
+app.config['SECRET_KEY'] = 'safjsafasfjks'
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
+class LoginForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login', render_kw={'class': 'btn custom-btn'})
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    form = LoginForm()
+    return render_template("index.html", form=form)
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5002)
+    app.run(debug=True, port=5002, host='172.16.249.198')
