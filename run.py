03b44730b5e3cfd44a557d1decff28b0306d2a58
DRANetGH35 <devanaptaker@gmail.com>
2025-04-26 18:44:45 -0700

    fix: get_ip function refactor

--- run.py
+++ run.py
@@ -5,6 +5,20 @@ app = create_app()
 
 from routes import *
 
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.254.254.254', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host=get_ip())
     #                              host='192.168.86.53'
